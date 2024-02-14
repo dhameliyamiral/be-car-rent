261bce28c3adb4f7ba2bdc120e7b40a5e50a74a6
miraldhameliya <dhameliyamiral@gmail.com>
2024-02-14 11:46:59 +0530

    add code

--- controller/userLoginApi.js
+++ controller/userLoginApi.js
@@ -1,6 +1,6 @@
 const userService = require("../services/registrationService");
 const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const userLoginApi = async (req, res) => {
   const { email, password } = req.body;
@@ -17,36 +17,34 @@ const userLoginApi = async (req, res) => {
           },
           process.env.jwt_secret_key
         );
-        return res.json({
-          status: "success",
-          message: "login success..!",
-          token,
-        });
         const transporter = nodemailer.createTransport({
-          host:process.env.EMAIL_HOST,
-          port:process.env.EMAIL_POST,
-          secure:false,
-          auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS,
+          host: process.env.EMAIL_HOST,
+          port: process.env.EMAIL_POST,
+          secure: false,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
           },
         });
         const mailOptions = {
           from: process.env.EMAIL_FROM,
           to: email,
           subject: "login informations",
-          text: 'Your login details:\nUsername: email\nPassword: password',
+          text: `Hello Your login details:\nUsername: ${email}\nPassword: ${password}`,
         };
-        console.log("mail option = ",mailOptions);
-        transporter.sendMail(mailOptions,(error,info)=>{
-          if(error){
+        console.log("mail option = ", mailOptions);
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
             console.log(error);
-          }
-          else
-          {
+          } else {
             console.log("email sent = " + info.response);
           }
-        })
+        });
+        return res.json({
+          status: "success",
+          message: "login success..!",
+          token,
+        });
       } else {
         return res.json({
           status: "failed",
