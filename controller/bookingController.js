7950816e694def830e00a71b99de1a0f8b9bdb2a
miraldhameliya <dhameliyamiral@gmail.com>
2024-03-19 10:12:47 +0530

    addcode

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -112,24 +112,25 @@ const bookingUpdate = async (req, res) => {
   res.json({ status: 200, message: "Update booking Successfully...!", data });
 };
 const bookingCancel = async (req, res) => {
-  const { car_id } = req.body;
+  const { booking_id } = req.body;
   try {
     const { id: user_id } = req.userData;
     date = new Date();
     const Booking = await bookingModel.findOne({
-      car_id: car_id,
-      user_id: user_id,
+      // car_id: car_id,
+      // user_id: user_id,
+      _id:booking_id
     });
     await bookingModel.findOneAndUpdate(
       { _id: Booking.id },
       { $set: { deletedAt: date } }
     );
     const data = await bookingModel.findOneAndUpdate(
-      { user_id: user_id },
+      { _id: booking_id},
       { $set: { status: "Cancel" } }
     );
     console.log("data ==", data);
-    res.json({ message: "Documnet Deleted Successfully..!!" });
+    res.json({ message: "Booking Cancle Successfully..!!" });
   } catch (error) {
     return res.json({ status: 500, message: "intrnal server error" });
   }
--- models/subscribModel.js
+++ models/subscribModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const subscribSchema = new mongoose.Schema({
     email:String
-});
+},{ timestamps: true });
 const subscribModel = mongoose.model("subsctibation",subscribSchema)
 module.exports = subscribModel
