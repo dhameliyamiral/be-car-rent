98b5a8f2b8f52de3416d9f1720e12a71cd6c16d4
miraldhameliya <dhameliyamiral@gmail.com>
2024-03-29 10:30:17 +0530

    addcpde

--- middlewares/userAuthMiddlewares.js
+++ middlewares/userAuthMiddlewares.js
@@ -4,7 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 let token;
 const userAuthMiddlewares = async (req, res, next) => {
-  const { authorization } = req.headers.authorization;
+  try {
+    const { authorization } = req.headers;
   if (authorization && authorization.startsWith("Bearer")) {
     token = authorization.split(" ")[1];
     const userTokenData = jwt.verify(token, process.env.jwt_secret_key);
@@ -15,14 +16,23 @@ const userAuthMiddlewares = async (req, res, next) => {
         req.userData = {
           id: user._id,
           email: user.email,
+      
         };
+        return next();
       } else {
         res.json({ status: 400, message: "you are not authorizeed" });
       }
     } else {
       res.json({ status: 400, message: "you are not authorizeed" });
     }
-    next();
+  } else {
+    return res.status(400).json({ status: 400, message: "Authorization header missing or invalid" });
+  }
+    // next();
+  } catch (error) {
+    console.error("Token verification error:", error.message);
+    return res.status(401).json({ status: 401, message: "Unauthorized" });
   }
 };
+
 module.exports = { userAuthMiddlewares };
